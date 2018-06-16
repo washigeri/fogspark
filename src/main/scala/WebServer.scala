8f17d62e3b2c311e9b7dfea6bd4381dda6a50c58
washigeri <malek.elouerghi@gmail.com>
2018-06-16 12:56:53 -0400

    debut spark
    Signed-off-by: washigeri <malek.elouerghi@gmail.com>

--- src/main/scala/WebServer.scala
+++ src/main/scala/WebServer.scala
@@ -2,8 +2,9 @@ import akka.Done
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import akka.http.scaladsl.server.Directives
+import akka.http.scaladsl.server.{Directives, Route}
 import akka.stream.ActorMaterializer
+import org.apache.spark.{SparkConf, SparkContext}
 import spray.json.DefaultJsonProtocol
 
 import scala.collection.mutable.ListBuffer
@@ -13,8 +14,16 @@ import scala.io.StdIn
 
 object WebServer {
 
+  val Conf: SparkConf = new SparkConf().setAppName("FogSpark").setMaster("local[*]")
+  val CloudURL = "http://localhost:8090/data"
+
 
   def main(args: Array[String]): Unit = {
+
+    //start spark
+    val sc = new SparkContext(Conf)
+    sc.setLogLevel("ERROR")
+
 
     var dataIoTs: ListBuffer[DataIoT] = new ListBuffer()
     implicit val system: ActorSystem = ActorSystem("fog-spark-http")
@@ -30,16 +39,16 @@ object WebServer {
     }
 
     trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
-      implicit val dataIotFormat = MyJsonProtocol.DataIoTJsonFormat
+      implicit val dataIotFormat: MyJsonProtocol.DataIoTJsonFormat.type = MyJsonProtocol.DataIoTJsonFormat
     }
 
     class MyJsonService extends Directives with JsonSupport {
-      val route = {
+      val route: Route = {
         path("data") {
           post {
             entity(as[DataIoT]) { data =>
               val saved: Future[Done] = saveData(data)
-              onComplete(saved) { done =>
+              onComplete(saved) { _ =>
                 complete("OK")
               }
 
@@ -56,5 +65,6 @@ object WebServer {
       .flatMap(_.unbind()) // trigger unbinding from the port
       .onComplete(_ ⇒ system.terminate()) // and shutdown when done
     println(dataIoTs)
+    sc.stop()
   }
 }
